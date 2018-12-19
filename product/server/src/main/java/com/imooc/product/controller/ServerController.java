c6bac0c41fced94e24df780e01f6b8eeddae1798
liminlei <minleili@126.com>
2018-12-19 14:59:42 +0800

    模块化

---
+++ product/server/src/main/java/com/imooc/product/controller/ServerController.java
@@ -0,0 +1,19 @@
+package com.imooc.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 廖师兄
+ * 2017-12-10 20:37
+ */
+@RestController
+@RequestMapping("/product")
+public class ServerController {
+
+    @GetMapping("/msg")
+    public String msg() {
+        return "this is product' msg";
+    }
+}
